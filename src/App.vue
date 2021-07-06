2db495caff0d75d483c92134cd908eb5bd9fc93b
LermeRomain <romainlerme@hotmail.fr>
2021-07-06 20:00:29 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -17,14 +17,22 @@ export default {
 
 <style>
 #app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   text-align: center;
-  padding: 10rem;
+  min-width: 100vw;
+  min-height: 100vh;
   background-color: #eb8a21;
+}
+
+*{
+  margin: 0;
+  padding: 0;
 }
 </style>
