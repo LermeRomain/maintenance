da2e14bbbfbff4ad7c7a411902b8b67a4d72f680
LermeRomain <romainlerme@hotmail.fr>
2021-07-06 19:55:31 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  bootstrap,
   render: h => h(App),
 }).$mount('#app')
